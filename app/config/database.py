17b79e635a11661a74bf391c10d17db950416daf
MakisVarv <christomvarv@yahoo.com>
2026-09-14 14:07:37 +0300

    add a completed BaseModel

--- app/config/database.py
+++ app/config/database.py
@@ -1,12 +1,35 @@
+import uuid
+from datetime import datetime
+
 from flask import Flask
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import DateTime, create_engine, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
 SessionLocal = sessionmaker()
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class BaseModel(Base):
+    __abstract__ = True
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
 
 
 def init_db(app: Flask) -> None:
